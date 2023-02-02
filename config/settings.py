7a905b370fb49800f4cd8010b20d61b55d4d9dd4
dilbazi <you@example.com>
2023-02-02 23:31:15 +0600

    lesson_4 New model

--- config/settings.py
+++ config/settings.py
@@ -31,9 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-lesson3
-   
-master
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -135,7 +132,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,7 +6,6 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
     path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
@@ -15,12 +14,3 @@ urlpatterns = [
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
     path("login/", views.LoginPageView.as_view(), name="login"),
 ]
-
-    path("", views.MainPageView.as_view()),
-    path("news/", views.NewsPageView.as_view()),
-    path("courses/", views.CoursesPageView.as_view()),
-    path("contacts/", views.ContactsPageView.as_view()),
-    path("doc_site/", views.DocSitePageView.as_view()),
-    path("login/", views.LoginPageView.as_view()),
-]
-
