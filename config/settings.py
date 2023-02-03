bdccb553ee5261afa7ea6516e9440f515ae6cd83
dilbazi <you@example.com>
2023-02-03 18:19:25 +0600

    lesson_4 Display model's data

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
     "mainapp",
 ]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
